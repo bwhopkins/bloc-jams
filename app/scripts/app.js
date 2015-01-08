ccc5aee5330f23a8657a878e4e0c51bb305dbd37
Brandon Hopkins <hello@brandonwhopkins.com>
2015-01-07 22:56:48 -0700

    app.js and player bar fixes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,12 @@
- //require('./landing');
- //require('./album');
- //require('./collection');
- //require('./profile');
+ require('./landing');
+ require('./album');
+ require('./collection');
+ require('./profile');
  
  angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
    $scope.subText = "Turn the music up!";
 
-    $scope.subTextClicked = funciton() {
+    $scope.subTextClicked = function() {
  		$scope.subText += '!';
  	};
 
